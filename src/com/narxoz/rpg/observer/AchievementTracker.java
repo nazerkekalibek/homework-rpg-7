e2df30a1b623324a15acdda9cdcf4235f1439512
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-04-10 02:45:53 +0500

    Added AchievementTracker

---
+++ src/com/narxoz/rpg/observer/AchievementTracker.java
@@ -0,0 +1,9 @@
+package com.narxoz.rpg.observer;
+
+public class AchievementTracker implements GameObserver{
+    public void onEvent(GameEvent e){
+        if(e.getType()==GameEventType.BOSS_DEFEATED){
+            System.out.println("🏆 Achievement unlocked: Boss Slayer!");
+        }
+    }
+}
