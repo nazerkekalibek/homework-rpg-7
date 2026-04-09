73b2995ed465c826b679894222a4ec5c4fc2895c
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-04-10 03:13:41 +0500

    Added PartySupport

--- src/com/narxoz/rpg/observer/PartySupport.java
+++ src/com/narxoz/rpg/observer/PartySupport.java
@@ -3,7 +3,7 @@ package com.narxoz.rpg.observer;
 public class PartySupport implements GameObserver{
     public void onEvent(GameEvent e){
         if(e.getType()==GameEventType.HERO_LOW_HP) {
-            System.out.println("✨ Party support activated!");
+            System.out.println("Party support activated!");
         }
     }
 }
